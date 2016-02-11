30a82c066b0a4410a8c16c719241962691489a07
Michael <Michael@131.159.193.196>
2016-02-11 19:41:32 +0100

    assign Warehouses To Orders

---
+++ src/Setup.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+import Model.Order;
+import Model.Warehouse;
+
+public class Setup {
+
+	public static void assignWarehousesToOrders(ArrayList<Warehouse> warehouses, ArrayList<Order> orders) {
+		for (Order order : orders) {
+			Warehouse nearestWarehouse = null;
+			int distanceToNearestWarehouse = Integer.MAX_VALUE;
+			for (Warehouse warehouse : warehouses) {
+				int distance = (int) Math.ceil(Math.sqrt(Math.pow(Math.abs(order.getPosX() - warehouse.getPosX()),2) + Math.pow(Math.abs(order.getPosY() - warehouse.getPosY()),2)));
+				if (distance < distanceToNearestWarehouse) {
+					nearestWarehouse = warehouse;
+					distanceToNearestWarehouse = distance;
+				}
+			}
+			if (nearestWarehouse != null) {
+				order.setWarehouse(nearestWarehouse);
+			} else {
+				System.out.println("No warehoues in input - this should not happen");
+			}
+		}
+	}	
+}
