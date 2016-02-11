5cfedc670c3284cb0a5ffe20e6acab194e352732
Michael <Michael@131.159.193.196>
2016-02-11 21:38:44 +0100

    Parsing

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,6 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
-
 public class Main {
 
-	public static void main(String[] args)  {
-		try {
-			URL path = Main.class.getResource("input.txt");
-			File f = new File(path.getFile());
-			final BufferedReader reader = new BufferedReader(new FileReader(f));
-			 String str;
-			while ((str = reader.readLine()) != null) {
-				// Parse input
-				System.out.println(str);
-			}
-			System.out.println(str);
-			reader.close();
-		}
-		catch (IOException e) {
-		}
+	public static void main(String[] args) {
+		Parser.parse();
 	}
 }
-
---
+++ src/Parser.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import Model.Environment;
+import Model.Order;
+import Model.Warehouse;
+
+public class Parser {
+
+	private static String path = "C:/Users/Michael/Downloads/busy_day.in";
+
+	public static void parse() {
+		try {
+			File file = new File(path);
+			Scanner scanner = new Scanner(file);
+			int amountOfLines = 0;
+			while (scanner.hasNextLine()) {
+				String[] line = scanner.nextLine().split("\\s+");
+				int amountOfWarehouses = -1;
+				int amountOfOrders = -1;
+
+				if (amountOfLines == 0) {
+					if (line.length != 5) {
+						System.out.println("THIS SHOULD NOT HAPPEN THIS IS THE FIRST LINE AND MUST HAVE 5 WORDS");
+					} else {
+						Environment.ROWS = Integer.parseInt(line[0]);
+						Environment.COLUMNS = Integer.parseInt(line[1]);
+						// ToDo: Parse Drones
+						Environment.TURNS = Integer.parseInt(line[3]);
+						Environment.PAYLOAD = Integer.parseInt(line[4]);
+					}
+				} else if (amountOfLines == 1) {
+					// Do nothing
+				} else if (amountOfLines == 2) {
+					int id = 0;
+					for (String weight : line) {
+						Environment.PRODUCT.put(id, Integer.parseInt(weight));
+						id++;
+					}
+				} else if (amountOfLines == 3) {
+					amountOfWarehouses = Integer.parseInt(line[0]);
+					for (int i = 0; i < amountOfWarehouses; i++) {
+						String[] warehousePosition = scanner.nextLine().split("\\s+");
+						int posX = Integer.parseInt(warehousePosition[0]);
+						int posY = Integer.parseInt(warehousePosition[1]);
+						String[] warehouseProducts = scanner.nextLine().split("\\s+");
+						HashMap<Integer, Integer> products = new HashMap<Integer, Integer>();
+						int id = 0;
+						for (String product : warehouseProducts) {
+							products.put(id, Integer.parseInt(product));
+							id++;
+						}
+						Environment.WAREHOUSES.add(new Warehouse(posX, posY, products));
+					}
+				} else {
+					amountOfOrders = Integer.parseInt(line[0]);
+					for (int i = 0; i < amountOfOrders; i++) {
+						String[] orderPosition = scanner.nextLine().split("\\s+");
+						int posX = Integer.parseInt(orderPosition[0]);
+						int posY = Integer.parseInt(orderPosition[1]);
+						scanner.nextLine().split("\\s+"); // skip
+						String[] products = scanner.nextLine().split("\\s+");
+						HashMap<Integer, Integer> productsHashMap = new HashMap<Integer, Integer>();
+						int totalWeight = 0;
+						for (String product : products) {
+							int id = Integer.parseInt(product);
+							if (productsHashMap.containsKey(id)) {
+								productsHashMap.put(id, productsHashMap.get(id) + 1);
+							} else {
+								productsHashMap.put(id, 1);
+							}
+							totalWeight += Environment.PRODUCT.get(id);
+						}
+						Environment.ORDERS.add(new Order(posX, posY, totalWeight, productsHashMap));
+					}
+				}
+				amountOfLines++;
+			}
+			scanner.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
